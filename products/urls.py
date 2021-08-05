6a8166f2f738b4edda15eb8f0303177b014d7e13
dwenpark <dwenup@gmail.com>
2021-08-05 18:43:29 +0900

    Modify: View 3개로 세분화

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls  import path
 
-from .views       import ProductView
+from .views       import BrandView, TypeView, ProductView
 
 urlpatterns = [
+    path("/brand", BrandView.as_view()),
+    path("/type", TypeView.as_view()),
     path("/catalog", ProductView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -3,35 +3,49 @@ from django.http    import JsonResponse
 
 from .models        import Brand, Type, Product
 
+class BrandView(View):
+    def get(self, request):
+        brands = Brand.objects.all()
+
+        catalog = [{
+            "id": brand.id,
+            "name": brand.name,
+            "image": brand.image
+        } for brand in brands]
+
+        return JsonResponse({"catalog": catalog}, status=200, safe=False)
+
+class TypeView(View):
+    def get(self, request):
+        types = Type.objects.all()
+
+        catalog = [{
+            "id": type.id,
+            "name": type.name,
+            "image": type.image
+        } for type in types]
+
+        return JsonResponse({"catalog": catalog}, status=200, safe=False)
+
 class ProductView(View):
     def get(self, request):
         filter   = request.GET.get("filter")
         id       = request.GET.get("id")
-        catalogs = []
 
         if filter == "brand":
-            brands   = Brand.objects.all()
-            id       = id if id else brands[0].id
+            id       = id if id else Brand.objects.all()[0].id
             products = Product.objects.filter(brand=id)
 
-            catalogs = [{
-                    "id"    : brand.id,
-                    "name"  : brand.name,
-                    "image" : brand.image
-                } for brand in brands]
-
         elif filter == "type":
-            types    = Type.objects.all()
-            id       = id if id else types[0].id
+            id       = id if id else Type.objects.all()[0].id
             products = Product.objects.filter(type=id)
-
-            catalogs = [{
-                    "id"    : type.id,
-                    "name"  : type.name
-                } for type in types]
 
         elif filter == "best":
             products = Product.objects.all().order_by("-like_number")
+
+        elif filter == "main":
+            id       = Brand.objects.all()[0].id
+            products = Product.objects.filter(brand=id).order_by("-like_number")
 
         else:
             products = Product.objects.all()
@@ -44,4 +58,4 @@ class ProductView(View):
                 "like_number" : product.like_number
             } for product in products]
 
-        return JsonResponse({"catalogs" : catalogs if catalogs else None, "items" : items}, status=200, safe=False)
+        return JsonResponse({"items" : items}, status=200, safe=False)

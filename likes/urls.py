78c48cd1f3d21f1de9ab015cb779f8c63dcaa8b9
dwenpark <dwenup@gmail.com>
2021-08-09 18:51:01 +0900

    Add: 찜하기 CRD view

--- likes/urls.py
+++ likes/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from .views      import LikesView
+
 urlpatterns = [
+    path("", LikesView.as_view()),
+    path("<int:product_id>", LikesView.as_view())
 ]
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views    import View
+from django.http     import JsonResponse
 
-# Create your views here.
+from products.models import Product
+from members.utils   import login_decorator
+from .models         import Like
+
+class LikesView(View):
+    @login_decorator
+    def get(self,request):
+        results = [{
+            "name"      : like.product.name,
+            "price"     : like.product.price,
+            "thumbnail" : like.product.thumbnail
+        } for like in Like.objects.filter(member_id=request.member)]
+
+        return JsonResponse({"Result": results}, status=200)
+
+    @login_decorator
+    def post(self, request, product_id):
+        if Like.objects.filter(member_id=request.member, product_id=product_id).exists():
+            return JsonResponse({"ERROR": "ALREADY_LIKED"}, status=400)
+
+        Like.objects.create(member_id=request.member, product_id=product_id)
+
+        product = Product.objects.get(id=product_id)
+        product.like_number += 1
+        product.save()
+        return JsonResponse({"Result": "LIKED"}, status=200)
+
+    @login_decorator
+    def delete(self, request, product_id):
+        if not Like.objects.filter(member_id=request.member, product_id=product_id).exists():
+            return JsonResponse({"ERROR": "DID_NOT_LIKED"}, status=400)
+
+        Like.objects.filter(member_id=request.member, product_id=product_id).delete()
+
+        product = Product.objects.get(id=product_id)
+        product.like_number -= 1
+        product.save()
+        return JsonResponse({"Result": "UNLIKED"}, status=200)
